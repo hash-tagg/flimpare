999c0f11dbb5c16422513e52b654b8865f05a3ac
hash-tagg <ashu2612yadav@gmail.com>
2023-02-05 12:37:46 +0530

    components setup

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Actors from "./actors/Actors";
+import Movies from "./movies/Movies";
+import Profile from "./profile/Profile";
+import MovieInfo from "./MovieInformation/MovieInfo";
+import Navbar from "./navbar/Navbar";
+
+export { Actors, Movies, Profile, MovieInfo, Navbar };
